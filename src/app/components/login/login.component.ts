3c987295dbcab2706517a782a719ad9f2c47b7f6
PEYRAT ALEXANDRE <APE18558@open-groupe.com>
2019-06-24 14:56:56 +0200

    Logout, Authguard, User registration

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+import { auth } from 'firebase';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(private authService: AuthService,
+              private flashMessage: FlashMessagesService,
+              private router: Router) {}
 
   ngOnInit() {
+
+// tslint:disable-next-line: no-shadowed-variable
+    this.authService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.router.navigate(['/']);
+      }
+    });
   }
 
+  onSubmit() {
+    this.authService.login(this.email, this.password)
+      .then(res => {
+        this.flashMessage.show('You are now logged in', {
+          cssClass: 'alert-success',
+          timout: 4000
+        });
+        this.router.navigate(['/']);
+      })
+      .catch(err => {
+        this.flashMessage.show(err.message, {
+          cssClass: 'alert-danger',
+          timout: 4000
+        });
+      });
+  }
 }
